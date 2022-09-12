1ca4a7c3de99e1ff4fd47cc42665681a8b562053
caiji <caiji202111@gmail.com>
2022-09-13 00:19:04 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  $('.mobileMenu-switch').on('click',function(){
+    $('.menu').addClass('open');
+    $('.mobileMenu-switch').addClass('d-none');
+    $('.mobileMenu-switch').css('display','none');
+    $('.mobileMenu-close').removeClass('d-none');
+  })
+  $('.mobileMenu-close').on('click',function(){
+    $('.menu').removeClass('open');
+    $('.mobileMenu-switch').removeClass('d-none');
+    $('.mobileMenu-switch').css('display','block');
+    $('.mobileMenu-close').addClass('d-none');
+  })
+
+  $(window).resize(function(){
+   const width=$(window).width()+17;
+  if(width < 768){
+    $('.mobileMenu-switch').removeClass('d-none');
+    $('.mobileMenu-switch').css('display','block');
+  }else{
+    $('.mobileMenu-switch').addClass('d-none');
+    $('.menu').removeClass('open');
+    $('.mobileMenu-close').addClass('d-none');
+    $('.mobileMenu-switch').css('display','none');
+  }
+ })
+});
